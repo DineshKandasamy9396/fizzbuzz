f593c24e821fdcb973ae7e9ac535984dea8535b4
DineshKandasamy9396 <dn365129@dal.ca>
2019-05-23 12:00:18 -0300

    task4 is modified

--- openbookfinal-2019-DineshKandasamy9396/Task4.py
+++ openbookfinal-2019-DineshKandasamy9396/Task4.py
@@ -23,7 +23,31 @@ def get_interface():
 
  print(r_json["response"])
 
+def get_network_count():
+
+ controller='sandboxapic.cisco.com'
+ #controller='devnetapi.cisco.com/sandbox/apic_em'
+
+ # put the ip address or dns of your apic-em controller in this url
+ url = "https://" + controller + "/api/v1/network-device/count"
+
+ #the username and password to access the APIC-EM Controller
+ """ payload = {"username":"devnetuser","password":"Cisco123!"}"""
+
+ #Content type must be included in the header
+ header = {"content-type": "application/json","X-Auth-Token":Auth_tok$
+
+ #Performs a POST on the specified url to get the service ticket
+ response= requests.get(url, headers=header, verify=False)
+
+ #convert response to json format
+ r_json=response.json()
+ 
+ print(r_json)
+
 ticket = __import__("create-ticket")
 import requests
 import json
 get_interface()
+get_network_count()
+
